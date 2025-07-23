03d4b95be1efcccb8bbffe7b46013a280cfe4057
Pritam Makwana <pritammakwana17561@gmail.com>
2025-07-23 10:41:32 +0530

    first commit

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <ul>
+        <li>
+          <NuxtLink to="/"> Home</NuxtLink>
+          <NuxtLink to="/about-us"> About Us</NuxtLink>
+          <NuxtLink to="/events"> Events</NuxtLink>
+          <NuxtLink to="/events/10"> EventsId</NuxtLink>
+          <NuxtLink to="/events/profile"> Profile</NuxtLink>
+        </li>
+      </ul>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import {resolve} from "path"; 
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  alias: {
+    '@': resolve(__dirname, './'),
+  },
+  css: ['@/assets/main.css'],
+ vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  
+})
